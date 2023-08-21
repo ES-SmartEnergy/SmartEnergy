a75ced6bb51ddd5b45dd0202cf173470bf772c2e
Wellington Jhonney <89779113+Wjhonney@users.noreply.github.com>
2023-08-20 23:33:23 -0300

    first commit
    criação de um projeto Node e React

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+//import './App.css';
+import GlobalStyle from "./styles/global";
+import "react-toastify/dist/React-toastify.css";
+
+function App() {
+  return (
+    <>
+      <ToastContainer autoClose={2000} position={}
+      <GlobalStyle/>
+    </>
+  );
+}
+
+export default App;
