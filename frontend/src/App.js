18303439a44afe8c642246690987e3a1957b3b1f
Wellington Jhonney <89779113+Wjhonney@users.noreply.github.com>
2023-08-21 19:28:55 -0300

    inclusão de algumas coisas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 //import logo from './logo.svg';
 //import './App.css';
 import GlobalStyle from "./styles/global";
-import "react-toastify/dist/React-toastify.css";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid.js";
+import { toast, ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Title = styled.h2``;
 
 function App() {
   return (
     <>
-      <ToastContainer autoClose={2000} position={}
+      <Container>
+        <Title>USUÁRIOS</Title>
+        <Form/>
+        <Grid/>
+      </Container>
+      <ToastContainer autoClose={2000} position={toast.POSITION.BOTTON_LEFT}/>
       <GlobalStyle/>
     </>
   );
